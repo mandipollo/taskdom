b497316d053eb412ad5b8d52cafd0d4f30f0129f
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2024-02-06 04:15:08 +0000

    drag and drop todo list done

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -48,6 +48,67 @@ const Task = () => {
 			);
 		}
 	};
+
+	// handle drag start
+
+	const handleDragStart = (e: React.DragEvent, id: string, list: string) => {
+		e.dataTransfer.setData("text/plain", id);
+		e.dataTransfer.setData("list", list);
+	};
+	// handle drag over
+
+	const handleDragOver = (e: React.DragEvent) => {
+		e.preventDefault();
+	};
+	// handle drop
+
+	const handleDrop = (e: React.DragEvent, targetList: string) => {
+		e.preventDefault();
+
+		const taskId = e.dataTransfer.getData("text/plain");
+		const sourceList = e.dataTransfer.getData("list");
+
+		// move task to new list
+
+		if (taskId && sourceList && targetList !== sourceList) {
+			const task =
+				sourceList === "todoList"
+					? todoList.find(task => task.id === taskId)
+					: sourceList === "progressList"
+					? progressList.find(progress => progress.id === taskId)
+					: completeList.find(complete => complete.id === taskId);
+
+			if (task) {
+				switch (targetList) {
+					case "todoList":
+						setTodoList(prev => [...prev, task]);
+						break;
+
+					case "progressList":
+						setProgressList(prev => [...prev, task]);
+						break;
+					case "completeList":
+						setCompleteList(prev => [...prev, task]);
+						break;
+					default:
+						break;
+				}
+			}
+
+			// remove task from the sourceList
+
+			switch (sourceList) {
+				case "todoList":
+					setTodoList(prev => prev.filter(task => taskId !== task.id));
+					break;
+				case "progressList":
+					setProgressList(prev => prev.filter(task => task.id !== taskId));
+					break;
+				default:
+					break;
+			}
+		}
+	};
 	return (
 		<div className="flex flex-col h-full w-full ">
 			<div className="flex justify-center items-center h-20 ">
@@ -65,20 +126,26 @@ const Task = () => {
 				</form>
 			</div>
 			<div className="grid grid-cols-3 h-full  ">
-				<div className="flex ">
+				<div
+					className="flex "
+					onDrop={e => handleDrop(e, "todoList")}
+					onDragOver={handleDragOver}
+				>
 					<ul className="flex items-center pt-2 flex-col w-full space-y-4">
 						<li className="flex w-5/6 h-10 ">TODO</li>
 						{todoList.map(todo => (
 							<li
+								draggable
+								onDragStart={e => handleDragStart(e, todo.id, "todoList")}
 								key={todo.id}
-								className="flex items-center justify-between w-5/6 h-10 border-[#30363E] bg-[#161B22] border rounded-md"
+								className=" flex items-center justify-between w-5/6 h-10 border-[#30363E] bg-[#161B22] border rounded-md"
 							>
-								<p className="pl-2 flex justify-center items-center">
+								<p className="w-3/4 overflow-hidden pl-2 flex items-center text-ellipsis whitespace-nowrap">
 									{todo.title}
 								</p>
 								<button
 									onClick={() => handleMoveToProgress(todo.id)}
-									className="p-2 bg-orange-400 rounded-sm"
+									className="p-2 h-full w-1/4 bg-orange-400 rounded-sm"
 								>
 									To do
 								</button>
@@ -86,29 +153,41 @@ const Task = () => {
 						))}
 					</ul>
 				</div>
-				<div className="flex ">
+				<div
+					className="flex "
+					onDragOver={handleDragOver}
+					onDrop={e => handleDrop(e, "progressList")}
+				>
 					<ul className="flex items-center pt-2 flex-col w-full space-y-4">
 						<li className="flex w-5/6 h-10 ">In progress</li>
 
 						{progressList.map(progress => (
 							<li
+								draggable
+								onDragStart={e =>
+									handleDragStart(e, progress.id, "progressList")
+								}
 								key={progress.id}
 								className="flex items-center justify-between w-5/6 h-10 border-[#30363E] bg-[#161B22] border rounded-md"
 							>
-								<p className="pl-2 flex justify-center items-center">
+								<p className="pl-2 flex  items-center text-ellipsis whitespace-nowrap w-3/4 overflow-hidden">
 									{progress.title}
 								</p>
 								<button
 									onClick={() => handleMoveToComplete(progress.id)}
-									className="p-2 bg-blue-400 rounded-sm"
+									className="p-2 bg-blue-400 rounded-sm w-1/4 h-full"
 								>
-									In progress
+									Progress
 								</button>
 							</li>
 						))}
 					</ul>
 				</div>
-				<div className="flex">
+				<div
+					className="flex"
+					onDragOver={handleDragOver}
+					onDrop={e => handleDrop(e, "completeList")}
+				>
 					<ul className="flex items-center pt-2 flex-col w-full space-y-4">
 						<li className="flex w-5/6 h-10 ">Complete</li>
 
@@ -117,10 +196,12 @@ const Task = () => {
 								key={complete.id}
 								className="flex items-center justify-between w-5/6 h-10 border-[#30363E] bg-[#161B22] border rounded-md"
 							>
-								<p className="pl-2 flex justify-center items-center">
+								<p className="pl-2 flex items-center text-ellipsis w-3/4 overflow-hidden">
 									{complete.title}
 								</p>
-								<button className="p-2 bg-blue-400 rounded-sm">Complete</button>
+								<button className="p-2 bg-blue-400 rounded-sm w-1/4 h-full">
+									Complete
+								</button>
 							</li>
 						))}
 					</ul>
