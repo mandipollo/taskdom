83c58b55e8739bc5826413e91bb0562b1c14dc0a
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2024-06-17 21:02:46 +0100

    minor updates

--- firebase.config.ts
+++ firebase.config.ts
@@ -6,18 +6,18 @@ import { getFirestore, writeBatch } from "firebase/firestore";
 
 import "firebase/functions";
 import { getFunctions } from "firebase/functions";
-console.log("API Key:", import.meta.env.VITE_APP_ID);
+
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_API_KEY,
 	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	databaseURL: import.meta.env.VITE_DATABASE_URL,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 	appId: import.meta.env.VITE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,26 +4,13 @@
 
 interface ImportMetaEnv {
 	readonly VITE_API_KEY: string;
-	readonly VITE_AUTH_DOMAIN: string;
+	readonly VITE_DATABASE_URL: string;
 	readonly VITE_PROJECT_ID: string;
 	readonly VITE_STORAGE_BUCKET: string;
 	readonly VITE_MESSAGING_SENDER_ID: string;
+	readonly VITE_MEASUREMENT_ID: string;
 	readonly VITE_APP_ID: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
-
-interface ImportMetaEnv {
-	readonly VITE_FIREBASE_API_KEY: string;
-	readonly VITE_FIREBASE_DATABASE_URL: string;
-	readonly VITE_FIREBASE_PROJECTID: string;
-	readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-	readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-	readonly VITE_FIREBASE_APP_ID: string;
-	readonly VITE_FIREBASE_MEASUREMENT_ID: string;
-	readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+	readonly VITE_AUTH_DOMAIN: string;
 }
 
 interface ImportMeta {
